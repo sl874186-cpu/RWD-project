3159a600565e39b5c8dd81365269e16b2b922a0b
sl874186-cpu <sl874186@gmail.com>
2026-07-23 20:33:00 +0800

    cart complete

--- js/all.js
+++ js/all.js
@@ -1,7 +1,25 @@
-/*burger menu for phone size*/
+
 $(document).ready(function () {
+    /*burger menu for phone size*/
     $(".button").click(function(e){
         e.preventDefault();
         $("body").toggleClass("active");
     });
+
+    /*cart sidebar*/
+    $(".sidebar a").click(function(e){
+        e.preventDefault();
+        $(this).toggleClass("active").parents().siblings().find("a").removeClass("active");
+    });
+
+    /*cart items heart*/ 
+    $(".heart-btn a").click(function(e){
+    e.preventDefault();
+    $(this).parent().parent().addClass("open");
+    });
+    $(".heart-btn-open a").click(function(e){
+    e.preventDefault();
+    $(this).parent().parent().removeClass("open");
+    });
 });
+
